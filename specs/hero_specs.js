cf5e4418947de805618378fe27d03e4f28dbe10f
Colin McKean <colinmckeandigital@gmail.com>
2017-03-22 15:31:03 +0000

    add favorite food test

--- specs/hero_specs.js
+++ specs/hero_specs.js
@@ -6,11 +6,14 @@ beforeEach(function () {
 })
 
 describe('The Hero', function () {
-  hero = new Hero("Colin", 100);
+  hero = new Hero("Colin", 100, "Chicken");
   it("hero should have a name of Colin", function () {
     assert.equal("Colin", hero.name);
   });
   it("hero should have 100 health to start", function () {
     assert.equal(100, hero.health);
   });
+  it("hero should have a favorite food of chicken", function(){
+    assert.equal("Chicken", hero.favoriteFood);
+  })
 })
