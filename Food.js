f351e296072f20152da4085494580de177d650bd
Colin McKean <colinmckeandigital@gmail.com>
2017-03-22 15:46:36 +0000

    pass test that food has replenishment value

--- Food.js
+++ Food.js
@@ -1,6 +1,6 @@
-Food = function(food_name, health_value){
+Food = function(food_name, replenishment_value){
   this.food_name = food_name;
-  this.health_value = health_value;
+  this.replenishment_value = replenishment_value;
 }
 
 module.exports = Food;
