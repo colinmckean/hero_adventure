6b1bf12504fcdb30892e7304b91afcdc13730969
Colin McKean <colinmckeandigital@gmail.com>
2017-03-22 15:35:25 +0000

    hero can pass the test of speak

--- Hero.js
+++ Hero.js
@@ -2,6 +2,13 @@ Hero = function(name, health, favoriteFood) {
   this.name = name;
   this.health = health;
   this.favoriteFood = favoriteFood;
+
+}
+
+Hero.prototype = {
+  speak: function(){
+    return "Hello, my name is " + this.name;
+  }
 }
 
 module.exports = Hero;
