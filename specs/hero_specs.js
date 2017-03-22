7b116f5e4ddba2fb443ec364fa0d1af11958263a
Colin McKean <colinmckeandigital@gmail.com>
2017-03-22 15:27:34 +0000

    add the hero heatlh test

--- specs/hero_specs.js
+++ specs/hero_specs.js
@@ -1,15 +1,16 @@
 var Hero = require('../Hero');
 var assert = require('assert');
 
-beforeEach(function(){
+beforeEach(function () {
   var hero;
-
 })
 
-describe('The Hero', function(){
-  hero = new Hero("Colin");
-  it("hero should have a name of Colin", function(){
+describe('The Hero', function () {
+  hero = new Hero("Colin", 100);
+  it("hero should have a name of Colin", function () {
     assert.equal("Colin", hero.name);
   });
-  
+  it("hero should have 100 health to start", function () {
+    assert.equal(100, hero.health);
+  });
 })
