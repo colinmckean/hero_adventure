76abbbcce6dcc03020bf5eb9c09930375fbad06a
Colin McKean <colinmckeandigital@gmail.com>
2017-03-22 15:31:31 +0000

    favorite food will pass the hero test.

--- Hero.js
+++ Hero.js
@@ -1,6 +1,7 @@
-Hero = function(name, health) {
+Hero = function(name, health, favoriteFood) {
   this.name = name;
   this.health = health;
+  this.favoriteFood = favoriteFood;
 }
 
 module.exports = Hero;
